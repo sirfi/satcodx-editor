4b853598d9b95af34fb96504e6ee20755ecb8be0
Fatih Işık <fatih_isik@windowslive.com>
2020-09-22 17:11:43 +0300

    name change feature added

--- src/App.vue
+++ src/App.vue
@@ -13,42 +13,92 @@
 
       <b-row>
         <b-col>
-          <h3>
-            Current Channel List
-            <b-button
-              variant="primary"
-              class="float-right"
-              @click="exportToSdxFile"
-            >Export to sdx file</b-button>
-          </h3>
-          <draggable
-            class="list-group"
-            :list="currentChannelList"
-            group="channelList"
-            @change="currentChannelListChanged"
-          >
-            <div
-              class="list-group-item"
-              v-for="element in currentChannelList"
-              :key="element.id"
-            >{{ element.name }}</div>
-          </draggable>
+          <b-card no-body>
+            <template v-slot:header>
+              <h4>
+                Current Channel List
+                <b-button
+                  variant="primary"
+                  class="float-right"
+                  @click="exportToSdxFile"
+                >Export to sdx file</b-button>
+              </h4>
+            </template>
+            <draggable
+              class="list-group"
+              :list="currentChannelList"
+              group="channelList"
+              @change="currentChannelListChanged"
+              handle=".handle"
+            >
+              <div class="list-group-item" v-for="channel in currentChannelList" :key="channel.id">
+                <channel-box
+                  :channel="channel"
+                  @remove="removeCurrentChannel"
+                  @name-changed="currentChannelListChanged"
+                ></channel-box>
+              </div>
+            </draggable>
+          </b-card>
         </b-col>
         <b-col>
           <div class="sticky" ref="tempChannelListStickyRef">
-            <h3>Temp Channel List</h3>
-            <draggable
-              class="list-group"
-              :list="tempChannelList"
-              group="channelList"
-              @change="tempChannelListChanged"
-            >
-              <div
-                class="list-group-item"
-                v-for="element in tempChannelList"
-                :key="element.id"
-              >{{ element.name }}</div>
-            </draggable>
+            <b-card no-body>
+              <template v-slot:header>
+                <h4>Temp Channel List</h4>
+              </template>
+              <draggable
+                class="list-group"
+                :list="tempChannelList"
+                group="channelList"
+                @change="tempChannelListChanged"
+                handle=".handle"
+              >
+                <div class="list-group-item" v-for="channel in tempChannelList" :key="channel.id">
+                  <channel-box
+                    :channel="channel"
+                    @remove="removeTempChannel"
+                    @name-changed="currentChannelListChanged"
+                  ></channel-box>
+                </div>
+              </draggable>
+            </b-card>
+            <b-card class="mt-2" no-body>
+              <template v-slot:header>
+                <h4>
+                  Removed Channel List
+                  <b-button
+                    variant="secondary"
+                    class="float-right"
+                    v-b-toggle:removed-channel-list-collapse
+                  >
+                    <span class="when-open">Close</span>
+                    <span class="when-closed">Open</span>
+                  </b-button>
+                </h4>
+              </template>
+              <b-collapse id="removed-channel-list-collapse">
+                <draggable
+                  class="list-group"
+                  :list="removedChannelList"
+                  group="channelList"
+                  @change="removedChannelListChanged"
+                  handle=".handle"
+                >
+                  <div
+                    class="list-group-item"
+                    v-for="channel in removedChannelList"
+                    :key="channel.id"
+                  >
+                    <channel-box
+                      :channel="channel"
+                      @remove="removeRemovedChannel"
+                      @name-changed="removedChannelListChanged"
+                    ></channel-box>
+                  </div>
+                </draggable>
+              </b-collapse>
+            </b-card>
           </div>
         </b-col>
       </b-row>
@@ -61,11 +111,13 @@ import draggable from "vuedraggable";
 import satcodx from "./satcodx";
 import stickyfilljs from "stickyfilljs";
 import { saveAs } from "file-saver";
+import channelBox from "./ChannelBox";
 
 export default {
   name: "App",
   components: {
     draggable,
+    channelBox,
   },
   data() {
     var savedTextSdx = localStorage.getItem("textSdx") || "";
@@ -73,11 +125,14 @@ export default {
       JSON.parse(localStorage.getItem("currentChannelList") || "[]") || [];
     var savedTempChannelList =
       JSON.parse(localStorage.getItem("tempChannelList") || "[]") || [];
+    var savedRemovedChannelList =
+      JSON.parse(localStorage.getItem("removedChannelList") || "[]") || [];
     return {
       fileSdx: null,
       textSdx: savedTextSdx,
       currentChannelList: savedCurrentChannelList,
       tempChannelList: savedTempChannelList,
+      removedChannelList: savedRemovedChannelList,
     };
   },
   created() {
@@ -114,6 +169,12 @@ export default {
         JSON.stringify(this.tempChannelList)
       );
     },
+    removedChannelListChanged() {
+      localStorage.setItem(
+        "removedChannelList",
+        JSON.stringify(this.removedChannelList)
+      );
+    },
     exportToSdxFile() {
       var exportTextSdx = "";
       for (let index = 0; index < this.currentChannelList.length; index++) {
@@ -127,18 +188,47 @@ export default {
         exportTextSdxCharArray[i] = exportTextSdx.charCodeAt(i);
       }
       var blob = new Blob([exportTextSdxCharArray], { type: "text/plain" });
-      saveAs(blob, "channel-list-" + new Date().toISOString().replace(/[^0-9]/g,"").substr(0,12) + ".sdx");
+      saveAs(
+        blob,
+        "channel-list-" +
+          new Date()
+            .toISOString()
+            .replace(/[^0-9]/g, "")
+            .substr(0, 12) +
+          ".sdx"
+      );
+    },
+    removeCurrentChannel(channelId) {
+      var index = this.currentChannelList.findIndex((x) => x.id === channelId);
+      var removedChannelArray = this.currentChannelList.splice(index, 1);
+      this.removedChannelList.push(...removedChannelArray);
+      this.currentChannelListChanged();
+      this.removedChannelListChanged();
+    },
+    removeTempChannel(channelId) {
+      var index = this.tempChannelList.findIndex((x) => x.id === channelId);
+      var removedChannelArray = this.tempChannelList.splice(index, 1);
+      this.removedChannelList.push(...removedChannelArray);
+      this.tempChannelListChanged();
+      this.removedChannelListChanged();
+    },
+    removeRemovedChannel(channelId) {
+      var index = this.removedChannelList.findIndex((x) => x.id === channelId);
+      this.removedChannelList.splice(index, 1);
+      this.removedChannelListChanged();
     },
   },
 };
 </script>
 
 <style>
-#app {
-}
 .sticky {
   position: -webkit-sticky;
   position: sticky;
   top: 0;
+}
+.collapsed > .when-open,
+.not-collapsed > .when-closed {
+  display: none;
 }
 </style>
--- src/satcodx.js
+++ src/satcodx.js
@@ -3,7 +3,25 @@ function parseLine(line) {
     var lineNumbers = line.match(/[0-9]/g);
     return {
         id: lineNumbers.join(""),
-        name: (line.substr(43, 8) + line.substr(115, 12)).trim().replace(/\05/g,"").replace(/\0/g,""),
+        name: (line.substr(43, 8) + line.substr(115, 12)).trim().replace(/\05/g, "").replace(/\0/g, ""),
+        satelliteName: line.substr(10, 18),
+        channelType: line.substr(28, 1),
+        broadCastSystem: line.substr(29, 4),
+        frequency: line.substr(33, 9),
+        frequencyNumber: parseInt(line.substr(33, 6)),
+        polarization: line.substr(42, 1),
+        symbolRate: line.substr(69, 5),
+        fec: line.substr(74, 1),
+        vpid: line.substr(75, 4),
+        apid: line.substr(79, 4),
+        pcrp: line.substr(83, 4),
+        sid: line.substr(87, 5),
+        nid: line.substr(92, 5),
+        tsid: line.substr(97, 5),
+        language: line.substr(102, 3),
+        countryCode: line.substr(106, 2),
+        languageCode: line.substr(108, 3),
+        crypto: line.substr(111, 4),
         rawLine: line
     };
 }
@@ -11,11 +29,16 @@ function parseText(textSdx) {
     var lines = textSdx ? textSdx.match(/[^\r\n]+/g) : [];
     return lines.map(function (line) {
         return parseLine(line);
-    }).filter(function(line){
+    }).filter(function (line) {
         return line;
     });
 }
+function changeName(line, newName) {
+    newName = newName + " ".repeat(20 - newName.length);
+    return line.substr(0, 43) + newName.substr(0, 8) + line.substr(51, 64) + newName.substr(8, 12) + line.substr(127);
+}
 export default {
     parseLine,
-    parseText
+    parseText,
+    changeName
 }
